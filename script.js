cd90f9501553b4100c31e666baf79bbec81b0e99
JechDev <101315207+Jech-dev@users.noreply.github.com>
2024-05-02 01:04:35 +0800

    script.js

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 
-
-const screen = document.querySelector('.screen');
+const screen =document.querySelector('.screen');
 
 function buttonClick(value){
   if(isNaN(value)){
-    handSymbol(value);
+    handleSymbol(value);
   }else{
     handleNumber(value);
-    }
-    screen.innerText = buffer;
+  }
+  screen.innerText = buffer;
 }
 
 function handleSymbol(symbol){
@@ -20,15 +19,74 @@ function handleSymbol(symbol){
       buffer = '0';
       runningTotal = 0;
       break;
-      case '=':
+    case '=':
       if(previousOperator === null){
         return
       }
-      flushOperator(parseInt(buffer));
+      flushOperation(parseInt(buffer));
       previousOperator = null;
       buffer = runningTotal;
       runningTotal = 0;
       break;
-    case ''
-    }
+    case '←':
+      if(buffer.length ===1){
+        buffer = '0';
+      }else{
+        buffer = buffer.substring(0, buffer.
+          length - 1);
+      }
+      break;
+    case '+':
+    case '−':
+    case '×':
+    case '÷':
+      handleMath(symbol);
+      break;
+  }
 }
+
+function handleMath(symbol){
+  if(buffer === '0'){
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if(runningTotal === 0){
+    runningTotal = intBuffer;
+  }else{
+    flushOperation(intBuffer);
+  }
+  previousOperator = symbol;
+  buffer = '0';
+}
+
+
+function flushOperation(intBuffer){
+  if(previousOperator === '+'){
+    runningTotal += intBuffer;
+  }else if (previousOperator === '−'){
+    runningTotal -= intBuffer;
+  }else if(previousOperator === '×'){
+    runningTotal *= intBuffer;
+  }else if(previousOperator === '÷'){
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleNumber(numberString){
+  if(buffer === "0"){
+    buffer = numberString;
+  }else{
+    buffer += numberString;
+  }
+}
+
+function init(){
+  document.querySelector('.calc-buttons').
+  addEventListener('click', function (even){
+    buttonClick(event.target.innerText);
+  })
+}
+
+init();
